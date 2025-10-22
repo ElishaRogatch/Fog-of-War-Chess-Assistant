05d72f50b07bc9e2f4e1fba68c921f883d2b6973
Elisha Rogatch <79295246+ElishaRogatch@users.noreply.github.com>
2025-10-22 09:39:12 -0400

    Remove SQL database dependency
    Made sure that all files could run as intended without the database.
    Fix X button to actually work.
    Created a visibility function that determines visibility for the player.

--- game_over.py
+++ game_over.py
@@ -1,55 +1,32 @@
-import tkinter as tk
 from tkinter import messagebox
-import sqlite3
+import chess
 
 class GameOver: 
-    def __init__(self, root, cursor, connection, board):
+    def __init__(self, root, board):
         self.root = root
         self.board = board
-        self.connection = connection
-        self.cursor = cursor
 
     def quit_game(self):
-        # Resets the SQLite table
-        self.cursor.execute("DROP TABLE IF EXISTS chessboard")  # Safely drops the table if it exists
-        print("Table chessboard has been dropped")
-        self.connection.commit()
-        self.cursor.execute("DROP TABLE IF EXISTS captured")  # Safely drops the table if it exists
-        print("Table captured has been dropped")
-        self.connection.commit()
-
-        # Close the SQLite connection
-        self.connection.close()
-        print("SQLite connection closed")
 
         # Close the GUI window
         self.root.quit()
         print("Game ended")
 
-    def check_game_over(self, is_white_turn):
-        captured_list = []
-        self.cursor.execute("SELECT * FROM captured;")
-        results = self.cursor.fetchall()
-        # Print each row to the console
-        for captured in results:
-            print(captured[0])  # Prints each row from the database
-            captured_list.append(captured[0])
-
-        """Check if the game is over (checkmate, stalemate, etc.)."""
-        if len(captured_list) > 1 and (captured_list[-1] == 'K' or captured_list[-1] == 'k'):
-            winner = "Black" if not is_white_turn else "White"
-            messagebox.showinfo("Checkmate", f"{winner} wins!")
-            self.quit_game()
-            # print(self.moves)
-            return True
-        elif self.board.is_stalemate():
-            messagebox.showinfo("Stalemate", "It's a draw!")
-            self.quit_game()
-            # print(self.moves)
-            return True
-        elif self.board.is_insufficient_material():
-            messagebox.showinfo("Draw", "Insufficient material for checkmate!")
-            self.quit_game()
-            # print(self.moves)
-            return True
+    def check_game_over(self):
+        """Check if the game is over."""
+        game_outcome = self.board.outcome()
+        if game_outcome is not None:
+            if game_outcome.termination == chess.Termination.VARIANT_LOSS:
+                winner = "White" if game_outcome.winner else "Black"
+                messagebox.showinfo("King Captured", f"{winner} wins!")
+                self.quit_game()
+                return True
+            elif game_outcome.termination == chess.Termination.FIFTY_MOVES:
+                messagebox.showinfo("50 Move Rule", "It's a draw!")
+                self.quit_game()
+                return True
+            elif game_outcome.termination == chess. Termination.THREEFOLD_REPETITION():
+                messagebox.showinfo("Threefold Repetition", "It's a draw!")
+                self.quit_game()
+                return True
         return False
--- play_game.py
+++ play_game.py
@@ -1,9 +1,8 @@
 import chess
 import tkinter as tk
-from tkinter import messagebox
 
 class PlayGame:
-    def __init__(self, root, board, canvas, square_size, board_draw, connection, cursor, database, game_over, is_white_turn, engine, bias):
+    def __init__(self, root, board, canvas, square_size, board_draw, game_over, engine, bias):
         self.root = root
         # Chess board size
         self.board = board
@@ -11,11 +10,7 @@ class PlayGame:
         self.canvas = canvas
         self.square_size = square_size
         self.board_draw = board_draw
-        self.connection = connection
-        self.cursor = cursor
-        self.database = database
         self.game_over = game_over
-        self.is_white_turn = is_white_turn
         # Track dots for move indicators
         self.move_dots = []
         # Track selected square and moves
@@ -25,12 +20,7 @@ class PlayGame:
         self.turn_label = tk.Label(self.root, text="White's Turn", font=16)
         self.turn_label.pack()
         self.bias = bias
-
-    def update_suggest_button_state(self):
-        if self.is_white_turn:
-            self.suggest_move_button.config(state=tk.NORMAL)
-        else:
-            self.suggest_move_button.config(state=tk.DISABLED)
+        self.captured_pieces = []
 
     def start_engine(self): 
         # Run the engine
@@ -38,13 +28,13 @@ class PlayGame:
 
     # updates the button state so its enbaled or not
     def update_suggest_button_state(self):
-        if self.is_white_turn:
+        if self.board.turn:
             self.suggest_move_button.config(state=tk.NORMAL)
         else:
             self.suggest_move_button.config(state=tk.DISABLED)
 
     def update_turn_label(self):
-        current_turn = "White's Turn" if self.is_white_turn else "Black's Turn"
+        current_turn = "White's Turn" if self.board.turn else "Black's Turn"
         self.turn_label.config(text=current_turn)
         
     def on_square_click(self, event):
@@ -63,7 +53,7 @@ class PlayGame:
         if self.selected_square is None:
             # Select the piece if any
             piece = self.board.piece_at(clicked_square)
-            if piece and piece.color == self.is_white_turn:  # Ensure the player selects their own piece
+            if piece and piece.color == self.board.turn:  # Ensure the player selects their own piece
                 self.selected_square = clicked_square
                 # Display possible moves for the selected piece
                 self.show_possible_moves(clicked_square)
@@ -75,101 +65,25 @@ class PlayGame:
             else:
                 move = chess.Move(self.selected_square, clicked_square)
             if move in self.board.fow_legal_moves:
-                
-                # end of og onto new way to stop castling problem
-                # Check if this is a castling move:
-                if self.board.is_castling(move):
-                    self.handle_castling(move)
-                    if self.is_white_turn:
-                        self.database.move_list.append(move.uci())
-                else:
-                    self.capture_piece(clicked_square, self.selected_square)
-                    self.board.push(move)
-                    self.board_draw.update_pieces()
-                    if self.is_white_turn:
-                        self.database.move_list.append(move.uci())
-                    self.database.update_database(self.selected_square, clicked_square, self.is_white_turn)
-                
-                # updates the visibility part of the database for player 1's perspective only
-                # I have it set to not because where it is located it will see the next player 1's move options therefore after the piece is moved
-                if not self.is_white_turn:
-                    self.database.update_visibility_white(list(self.board.fow_legal_moves))
-                
-                if self.is_white_turn:
-                    self.database.update_visibility_black(list(self.board.fow_legal_moves))
-
+                captured_piece = self.board.piece_at(clicked_square)
+                if captured_piece: # if captured_piece is not None
+                    print(f"{chess.COLOR_NAMES[captured_piece.color]} {chess.PIECE_NAMES[captured_piece.piece_type]} was captured!")
+                    self.captured_pieces.append(captured_piece.symbol())
+                # make the move
+                self.board.push(move)
+                self.board_draw.update_pieces()
                 # Check for game-ending conditions
-                if self.game_over.check_game_over(self.is_white_turn):
+                if self.game_over.check_game_over():
                     return
-
+                
                 # Switch turns between players and functionalites
-                self.is_white_turn = not self.is_white_turn
                 self.update_suggest_button_state()
                 self.update_turn_label()
-                if self.is_white_turn:
-                    self.board_draw.draw_fog_white()
-                else: 
-                    self.board_draw.draw_fog_black()
-            else:
-                messagebox.showerror("Illegal Move", "That move is not legal.")
+                # draw the fog for the player
+                self.board_draw.draw_fog(list(self.board.fow_legal_moves))
 
             # Reset selected square
             self.selected_square = None
-
-    # Extra move is handled and then set to a seperate function that updates the board
-    def handle_castling(self, move):
-        # Push the castling move onto the board and update the GUI.
-        self.board.push(move)
-        self.board_draw.update_pieces()
-        
-        # Determine which castling move it is and update the database accordingly.
-        if self.is_white_turn:
-            if move.to_square == chess.G1:  # White kingside castling: King: E1 -> G1, Rook: H1 -> F1
-                self.update_database_castling('E1', 'G1', 'H1', 'F1', 'W')
-            elif move.to_square == chess.C1:  # White queenside castling: King: E1 -> C1, Rook: A1 -> D1
-                self.update_database_castling('E1', 'C1', 'A1', 'D1', 'W')
-        else:
-            if move.to_square == chess.G8:  # Black kingside castling: King: E8 -> G8, Rook: H8 -> F8
-                self.update_database_castling('E8', 'G8', 'H8', 'F8', 'B')
-            elif move.to_square == chess.C8:  # Black queenside castling: King: E8 -> C8, Rook: A8 -> D8
-                self.update_database_castling('E8', 'C8', 'A8', 'D8', 'B')
-
-    # updates the SQLite table to reflect the 2 move in 1 due to castling
-    def update_database_castling(self, king_from, king_to, rook_from, rook_to, color):
-        # Remove the king from its starting square
-        self.cursor.execute(
-            "UPDATE chessboard SET color = '', piece = '' WHERE col = ? AND rw = ?",
-            (king_from[0], int(king_from[1]))
-        )
-        # Place the king on its destination square
-        king_piece = 'K' if color == 'W' else 'k'
-        self.cursor.execute(
-            "UPDATE chessboard SET color = ?, piece = ? WHERE col = ? AND rw = ?",
-            (color, king_piece, king_to[0], int(king_to[1]))
-        )
-        # Remove the rook from its starting square
-        self.cursor.execute(
-            "UPDATE chessboard SET color = '', piece = '' WHERE col = ? AND rw = ?",
-            (rook_from[0], int(rook_from[1]))
-        )
-        # Place the rook on its destination square
-        rook_piece = 'R' if color == 'W' else 'r'
-        self.cursor.execute(
-            "UPDATE chessboard SET color = ?, piece = ? WHERE col = ? AND rw = ?",
-            (color, rook_piece, rook_to[0], int(rook_to[1]))
-        )
-        self.connection.commit()
-
-    def capture_piece(self, clicked_square, selected_square):
-        captured_piece = self.board.piece_at(clicked_square)
-        print("I made it here! I am going to capture", captured_piece)
-        if captured_piece: # if captured_piece is not None
-            #if capture_piece 
-            print("I actually captured", captured_piece, "here!")
-            self.cursor.execute("INSERT INTO captured (piece) VALUES (?)", (captured_piece.symbol()))
-
-        # if (self.board.piece_at(selected_square) == 'P' or self.board.piece_at(selected_square) == 'p') and (clicked_square >= 56 or clicked_square <= 7):
-        #     promote()
 
     def show_possible_moves(self, square):
         """Show dots on squares where the selected piece can move."""
